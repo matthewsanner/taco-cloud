df13f3f72747985a95e4cb64b131b4f96d464b8b
Matthew Sanner <matthewsannerdev@gmail.com>
2024-12-31 11:25:19 -0600

    adds ingred. converter, order form, validation

--- src/main/java/com/matthewsanner/taco_cloud/controller/DesignTacoController.java
+++ src/main/java/com/matthewsanner/taco_cloud/controller/DesignTacoController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import jakarta.validation.Valid;
+import org.springframework.validation.Errors;
 
 import lombok.extern.slf4j.Slf4j;
 import com.matthewsanner.taco_cloud.domain.Ingredient;
@@ -59,7 +61,11 @@ public class DesignTacoController {
   }
 
   @PostMapping
-  public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+  public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+    if (errors.hasErrors()) {
+      return "design";
+    }
+
     tacoOrder.addTaco(taco);
     log.info("Processing taco: {}", taco);
 
